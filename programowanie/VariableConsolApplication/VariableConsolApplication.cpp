9aecc0fc890df64e08dedb1fc3702066d282d6e3
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-25 09:53:25 +0200

    zmiany

---
+++ programowanie/VariableConsolApplication/VariableConsolApplication.cpp
@@ -0,0 +1,31 @@
+﻿// VariableConsolApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+void task2()
+{
+    float lenghtInMeterFromUser;
+    std::cout << "Podaj dlugosc w metrach. \n";
+    std::cin >> lenghtInMeterFromUser;
+
+    float lenghtInCentimeter; 
+    lenghtInCentimeter = lenhtInMeterFromUser / 100;
+
+}
+
+void task1()
+{
+    short ageFromUser;
+    std::cout << "Podaj swoj wiek. \n";
+    std::cin >> ageFromUser;
+
+    std::cout << "Masz " << ageFromUser << " lat. \n";
+
+}
+
+int main()
+{
+    //task1();
+    task2();
+ }
+
