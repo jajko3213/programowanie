90e575697a3ea03767ea447abfbd5753c3aa2617
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-11-29 09:50:16 +0100

    nowy

---
+++ programowanie/ConsoleApplication1/InstructionsIfConsoleApplication.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    task1();
+}
+
+void task1()
+{
+    int number;
+    std::cout << "Podaj liczbe \n";
+    std::cin >> number;
+
+    if (number > 0);
+    {  
+        std::cout << "Liczba jest dodatnia \n";
+    }
+
+    if (number < 0);
+    {
+
+    }
+} 
