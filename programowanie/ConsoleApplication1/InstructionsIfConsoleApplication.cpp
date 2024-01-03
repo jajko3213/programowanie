2e733120781f7dd39810feb652296b9c00022bcd
Uczeń - sala 213 <Ucze - sala 213@213-13>
2024-01-03 09:25:12 +0100

    cvxc

--- programowanie/ConsoleApplication1/InstructionsIfConsoleApplication.cpp
+++ programowanie/ConsoleApplication1/InstructionsIfConsoleApplication.cpp
@@ -59,11 +59,81 @@ void task2()
 
     //wersja 2
 
-    if(number >= && number
+    if(number >= && number)
 }
+
+//Napisz program ktory wczyta jedna liczbe i wyswietli wieksza z nich.
+
+void task3()
+{
+    int firstNumber;
+    std::cout << "Podaj liczbe\n";
+    std::cin >> firstNumber;
+
+    std::cout << "Najwieksza wartosc to" << firstNumber << "\n";
+}
+void task4()
+{
+    int firstNumber, secondNumber;
+    std::cout << "podaj liczbe\n";
+    std::cin >> firstNumber;
+    std::cout << "podaj liczbe\n";
+    std::cin >> secondNumber;
+
+    if (secondNumber > firstNumber)
+        std::cout << "Najwieksza wartosc to " << "\n";
+    else
+        std::cout << "najwieksza wartosc to " << firstNumber << "\n";
+    
+}
+
+void task6()
+{
+    int firstNumber, secondNumber, thirdNumber, fourthNumber;
+    std::cout << "podaj liczbe\n";
+    std::cin >> firstNumber;
+    std::cout << "podaj liczbe\n";
+    std::cin >> secondNumber;
+    std::cout << "podaj liczbe\n";
+    std::cin >> thirdNumber;
+    std::cout << "podaj liczbe\n";
+    std::cin >> fourthNumber;
+
+    if (fourthNumber > secondNumber
+        && fourthNumber > secondNumber
+        && fourthNumber > thirdNumber)
+        std::cout << "Największa wartość to " << fourthNumber << "\n";
+    else
+    {
+        if (thirdNumber > firstNumber && thirdNumber > secondNumber)
+            std::cout << "Największa wartość to " << thirdNumber << "\n";
+        else
+        {
+            if (secondNumber > firstNumber)
+                std::cout << "Najwieksza wartosc to " << secondNumber << "\n";
+            else
+                std::cout << "Najwieksza wartosc to " << firstNumber << "\n";
+        }
+    }
+}
+            
+
+
+
+
+
 
 int main()
 {
     //task1();
-    task2();
+    //task2();
+   //task3();
+    //task4();
+    //task5();
+    task6();
+
+
+
+
+
 }
