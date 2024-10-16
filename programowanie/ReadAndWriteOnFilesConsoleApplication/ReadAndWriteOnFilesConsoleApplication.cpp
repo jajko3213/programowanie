cdcf218836785e717204a42a74ce470343fab747
q <q>
2024-10-16 08:51:12 +0200

    gjhk

--- programowanie/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
+++ programowanie/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
@@ -3,30 +3,50 @@
 
 void task1()
 {
-    std::string name;
-    int age;
+	std::string name;
+	int age;
 
-    std::cout << "Podaj imie";
-    std::cin << name;
+	std::cout << "Podaj imie\n";
+	std::cin >> name;
 
-    std::cout << "Podaj wiek";
-    std::cin >> age;
+	std::cout << "Podaj wiek\n";
+	std::cin >> age;
 
-    std::ofstream writeInFile;
-    writeInFile.open("g:\\dane.txt");
+	std::ofstream writeInFile;
+	writeInFile.open("c:\\dane.txt", std::ios::app);
 
-    if (writeInFile.is_open() == true)
-    {
-        //praca na pliku
-        writeInFile << name;
-        writeInFile << "\n";
-        writeInFile << age;
+	if (writeInFile.is_open() == true)
+	{
+		//praca na pliku
+		writeInFile << name;
+		writeInFile << "\n";
+		writeInFile.flush();
+		writeInFile << age << "\n";
 
-        writeInFile.close();
-    }
+		writeInFile.close();
+	}
+	else
+		std::cout << "Błąd otwarcia pliku\n";
+}
+
+void task2()
+{
+	std::string name
+	int age;
+	std::ifstream readFromFile;
+	readFromFile.open("c://dane.txt");
+	if (readFromFile.is_open())
+	{
+		readFromFile +>> name;
+		readFromFile >> age;
+
+		std::cout << "Imie: " << name << "wiek: " << age << << "\n";
+
+		readFromFile.close();
+	}
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	task2();
 }
