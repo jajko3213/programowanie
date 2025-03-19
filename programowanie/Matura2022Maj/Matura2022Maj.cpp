9feb9674eaa5276bc12cbaf5aa1508a1808f29c9
q <q>
2025-03-19 08:53:51 +0100

    ffyufhjh

---
+++ programowanie/Matura2022Maj/Matura2022Maj.cpp
@@ -0,0 +1,66 @@
+﻿// Matura2022MajConsoleApplication.cpp :
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+/*
+1. Kopiujemy pliki txt do folderu z projektem
+2. PPM na pliku txt i wybieramy Properties.
+3. Zmieniamy Item type na "Copy to"
+*/
+
+int main()
+{
+	std::ifstream file("przyklad.txt");
+
+	/*
+	int numbers[200];
+
+	for (int i = 0; i < 200; i++)
+	{
+		file >> numbers[i];
+	}*/
+
+	std::vector<int> numbers;
+
+	int n;
+	while (file >> n)
+	{
+		numbers.push_back(n);
+	}
+
+
+	std::cout << "Odczytane liczby:\n";
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		std::cout << numbers[i] << ", ";
+	}
+
+	std::cout << "odzczytanie liczby";
+	for (int num : numbers)
+	{
+		std::cout << num << "\n";
+	}
+
+	std::cout << "\n";
+
+	int counter = 0;
+	for (int num : numbers)
+	{
+		int firstDigit;
+		int lastDigit = num % 10;
+
+		int tmpNum = num;
+		do
+		{
+			firstDigit = tmpNum % 10;
+			tmpNum = tmpNum / 10;
+		} while (tmpNum != 0);
+
+		if (firstDigit == lastDigit)
+		{
+			counter++
+		}
+		std::cout << counter << "\n";
+	}
+}
